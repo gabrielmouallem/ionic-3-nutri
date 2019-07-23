e215619fe21508e8e5382102f7b986c5f1ff1859
Gabriel <gabriel.unifei2017@gmail.com>
2019-07-23 11:36:15 -0300

    login conectado ao bd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,18 +5,26 @@ import { ToastController } from 'ionic-angular';
 import { DicasPage } from '../dicas/dicas';
 import { RegisterPage } from '../register/register';
 
+import { AngularFireAuth } from 'angularfire2/auth'
+
+import { Users } from './users';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  users: Users = new Users();
+
   @ViewChild("usuario") email;
   @ViewChild("senha") password;
 
   constructor(
     public navCtrl: NavController,
-    public toastCtrl: ToastController
+    public toastCtrl: ToastController,
+    public fire: AngularFireAuth
     ) {
 
   }
@@ -25,14 +33,30 @@ export class HomePage {
 
     let toast = this.toastCtrl.create({duration: 3000, position: 'bottom'});
 
-    if (this.email.value ==  "admin" && this.password.value == "admin"){
-      this.navCtrl.push(DicasPage);
-      toast.setMessage('Logado com sucesso!');
+    this.fire.auth.signInWithEmailAndPassword(this.email.value, this.password.value)
+    .then (data =>{
+
+      console.log ("dados do login:", data);
+      this.users.email = this.email.value;
+      this.users.senha = this.password.value;
+
+      this.navCtrl.setRoot(DicasPage);
+    })
+    .catch((error: any) => {
+      if (error.code == 'auth/user-disabled') {
+        toast.setMessage("Este usuário foi não se encontra mais em nosso banco.");
+      }
+      else if (error.code == 'auth/invalid-email') {
+        toast.setMessage("Email inválido.");
+      }
+      else if (error.code == 'auth/user-not-found') {
+        toast.setMessage("Usuário não encontrado.");
+      }
+      else if (error.code == 'auth/wrong-password') {
+        toast.setMessage("Senha incorreta.");
+      }
       toast.present();
-    } else {
-      toast.setMessage('Usuário ou senha não encontrado.');
-      toast.present();
-    }
+    });
     
   }
 
